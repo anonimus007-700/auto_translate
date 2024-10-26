6a34c281d23b65bcfa3634694263f37e656fc5be
anonimus007-700 <sahsafg80@gmail.com>
2024-10-27 02:32:39 +0300

    backup

--- auto_translate.py
+++ auto_translate.py
@@ -14,13 +14,18 @@ import os
 
 
 class AutoTranslate:
-    def __init__(self, url: str):
+    def __init__(self, url: str, _callback=None):
         self.url = url
+        self._callback = _callback
+        
+        self.folder_path = 'assets'
+        self.progress = 0
+        self.line_width = 7
     
-        self.language = 'ua'    # ua en
-        self.model_id = 'v4_ua' # v4_ua v3_en
-        self.sample_rate = 48000 # 8000, 24000, 48000
-        self.speaker = 'mykyta' # mykyta en_0
+        self.language = 'ua'      # ua en
+        self.model_id = 'v4_ua'   # v4_ua v3_en
+        self.sample_rate = 48000  # 8000, 24000, 48000
+        self.speaker = 'mykyta'   # mykyta en_0
         self.device = torch.device('cpu')
 
         self.model_silero, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
@@ -30,10 +35,11 @@ class AutoTranslate:
 
         self.model_silero.to(self.device)
         
-        self.model = whisper.load_model("base")
+        self.model = whisper.load_model('base')
         
         self.file_check()
         self.download_video()
+        self.separate_audio()
 
         self.recognize_audio()
         self.audize()
@@ -41,41 +47,88 @@ class AutoTranslate:
         self.fragments_orig()
         self.mix_audio()
     
+    def __callback(self, description: str, plus: bool=False):
+        if self._callback and plus:
+            self.progress += 1
+            self._callback({
+                    'progress': self.progress,
+                    'line_width': self.line_width,
+                    'description': description
+                })
+        elif self._callback:
+            self._callback({
+                    'progress': self.progress,
+                    'line_width': self.line_width,
+                    'description': description
+                })
+    
     def file_check(self):
-        files_to_check = ["output_video.mp4", "output.wav", "video.mp4", "audio.wav"]
+        description = 'Deleting old files'
+        self.__callback(description)
+
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+
+        files_to_check = ['output_video.mp4', 'video.mp4', 'audio.wav']
 
         for file in files_to_check:
-            if os.path.exists(file):
-                os.remove(file)
+            file_path = os.path.join(self.folder_path, file)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        
+        self.__callback(description, True)
 
     def download_video(self):
+        description = 'Download video'
+        self.__callback(description)
+        
         ydl_opts = {
             'format': 'best',
-            'outtmpl': 'video.%(ext)s',  # Save file as <title>.<ext>
+            'outtmpl': os.path.join(self.folder_path, 'video.%(ext)s')  # Save file as <title>.<ext>
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.url])
+
+        self.__callback(description, True)
+    
+    def separate_audio(self):
+        description = 'Separating audio'
+        self.__callback(description)
         
-        self.video = VideoFileClip("video.mp4")
+        self.output_video_path = os.path.join(self.folder_path, 'video.mp4')
+        self.video = VideoFileClip(str(self.output_video_path))
 
         audio = self.video.audio
-        audio.write_audiofile("audio.wav")
+
+        self.output_audio_path = os.path.join(self.folder_path, 'audio.wav')
+        audio.write_audiofile(self.output_audio_path)
+        
+        self.__callback(description, True)
     
 
     def recognize_audio(self):
-        result = self.model.transcribe("audio.wav", word_timestamps=True)
+        description = 'Word recognition'
+        self.__callback(description)
+        
+        result = self.model.transcribe(self.output_audio_path, word_timestamps=True)
 
         self.final = []
+        self.line_width += len(result['segments'])
         
         for segment in result['segments']:
-            start = segment['start'] + 0.1
+            start = segment['start']
             end = segment['end'] + 0.1
             text = segment['text']
             translated = GoogleTranslator(source='auto', target='uk').translate(text)
-            self.final.append({'timestamp': (float(f"{start:.2f}"), float(f"{end:.2f}")), 'text': translated})
+            self.final.append({'timestamp': (float(f'{start:.2f}'), float(f'{end:.2f}')), 'text': translated})
+        
+        self.__callback(description, True)
     
     def audize(self):
+        description = 'Voicing of fragments'
+        self.__callback(description)
+        
         chunks_data = [{'start_time': chunk['timestamp'][0], 'end_time': chunk['timestamp'][1], 'text': chunk['text']}\
                 for chunk in self.final]
         df = pd.DataFrame(chunks_data)
@@ -105,14 +158,21 @@ class AutoTranslate:
                 end_time = end_time + difference
 
             self.prev_end_time = end_time
+            
+            self.__callback(description, True)
 
             return {'start_time': start_time, 'end_time': end_time, 'audio': audio}
 
         self.alt = df.apply(generate_audio, axis=1).tolist()
 
+        self.__callback(description, True)
+
     def fragments_orig(self):
+        description = 'Fragments without words'
+        self.__callback(description)
+        
         chunks_data = []
-        waveform, sample_rate = torchaudio.load("audio.wav")
+        waveform, sample_rate = torchaudio.load(self.output_audio_path)
 
         for i, chunk in enumerate(self.alt):
             start_time = chunk['end_time']
@@ -142,48 +202,51 @@ class AutoTranslate:
             
         self.chunks = df.apply(get_samples, axis=1).tolist()
 
+        self.__callback(description, True)
+
     def mix_audio(self):
-        sample_rate = 48000
+        description = 'Mixing audio and video'
+        self.__callback(description)
         
         df1 = pd.DataFrame(self.alt)
         df2 = pd.DataFrame(self.chunks)
         
         df = pd.concat([df1, df2], ignore_index=True)
         df = df.sort_values(by=['start_time', 'end_time']).reset_index(drop=True)
-        
-        print(df)
-        
-        # df = df1
 
-        df['end_time'] = df['start_time'] + df['audio'].apply(lambda x: len(x) / sample_rate)
+        df['end_time'] = df['start_time'] + df['audio'].apply(lambda x: len(x) / self.sample_rate)
 
         total_duration = df['end_time'].max()
-        total_samples = int(total_duration * sample_rate)
+        total_samples = int(total_duration * self.sample_rate)
 
         combined_audio = torch.zeros(total_samples)
 
         for _, row in df.iterrows():
-            start_sample = int(row['start_time'] * sample_rate)
+            start_sample = int(row['start_time'] * self.sample_rate)
             audio_data = row['audio']
             end_sample = start_sample + len(audio_data)
 
             if end_sample > total_samples:
-                print(f"Warning: audio segment exceeds total duration at {row['start_time']}s.")
+                print(f'Warning: audio segment exceeds total duration at {row['start_time']}s.')
                 end_sample = total_samples
 
             combined_audio[start_sample:end_sample] = audio_data[:end_sample - start_sample]
 
-        torchaudio.save("output.wav", combined_audio.unsqueeze(0), sample_rate)
+        torchaudio.save(self.output_audio_path, combined_audio.unsqueeze(0), self.sample_rate)
 
-        audio = AudioFileClip("output.wav")
+        audio = AudioFileClip(self.output_audio_path)
+        
+        video_path = os.path.join(self.folder_path, 'output_video.mp4')
         
         video_with_audio = self.video.set_audio(audio)
-        video_with_audio.write_videofile("output_video.mp4", codec="libx264", audio_codec="aac")
+        video_with_audio.write_videofile(video_path, codec='libx264', audio_codec='aac')
+
+        self.__callback(description, True)
     
 
-if __name__ == "__main__":
-    # auto = AutoTranslate('https://www.youtube.com/shorts/Gno-zBED01Y')
-    auto = AutoTranslate('https://www.youtube.com/watch?v=PTAI7NXHX0I')
+if __name__ == '__main__':
+    auto = AutoTranslate('https://www.youtube.com/shorts/Gno-zBED01Y')
+    # auto = AutoTranslate('https://www.youtube.com/watch?v=PTAI7NXHX0I')
     # auto = AutoTranslate('https://www.youtube.com/watch?v=owePZqppYWM')
     print('Complite!')
 
--- main.py
+++ main.py
@@ -1,4 +1,81 @@
+import flet as ft
+
+from validators import url
 from auto_translate import AutoTranslate
 
-auto = AutoTranslate('https://www.youtube.com/shorts/Gno-zBED01Y')
-print('Complite!')
+import os
+
+# font_yokai_path = os.path.join("assets", "Yokai.otf")
+
+def main(page: ft.Page):
+    def progress_callback(e):
+        progress_ring.value = e['progress'] / e['line_width']
+        
+        if e['progress'] / e['line_width'] == 1:
+            page.close(progress_dlg)
+
+        page.update()
+
+    def submit(e):
+        url_text = url_field.value
+
+        if url(url_text):
+            page.open(progress_dlg)
+            auto = AutoTranslate(url_text, _callback=progress_callback)
+        else:
+            page.open(not_url)
+            
+        
+    page.theme_mode = ft.ThemeMode.DARK
+    page.title = "Auto Transtate"
+    page.window.always_on_top = True
+    page.spacing = 20
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.theme = ft.Theme(color_scheme_seed='indigo')
+
+    page.fonts = {
+        "Yokai": "Yokai.otf"
+    }
+    
+    url_field = ft.TextField(label="YouTube URL", hint_text="Please enter URL here")
+    submit_but = ft.ElevatedButton("Submit", on_click=submit)
+
+    not_url = ft.CupertinoAlertDialog(
+        title=ft.Text("URL is incorrect!"),
+        actions=[
+            ft.TextButton("OK", on_click=lambda e: page.close(not_url)),
+        ],
+    )
+    
+    progress_ring = ft.ProgressRing(semantics_label = 'zxc')
+    progress_dlg = ft.CupertinoAlertDialog(
+        modal=True,
+        content=ft.Container(
+            content=progress_ring,
+            width=150,
+            height=150,
+            padding=50,
+            alignment=ft.alignment.center,
+        ),
+    )
+
+    page.add(
+        ft.Row(
+            [
+                ft.Column(
+                    [
+                        ft.Text("Auto Transtate", font_family="Yokai", size=50),
+                        url_field,
+                        submit_but
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                )
+            ],
+            expand=True,
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(main, assets_dir="assets")
